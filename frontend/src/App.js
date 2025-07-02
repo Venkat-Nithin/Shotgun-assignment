d02cbd1abdaf4c766b0a12f9afe3d41bf8f2d946
Venkat-Nithin <venkatnithinm@gmail.com>
2025-07-02 19:19:21 +0530

    demo2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { io } from 'socket.io-client';
 
-const socket = io('http://localhost:4000');
+const socket = io(process.env.REACT_APP_SOCKET_SERVER || 'http://localhost:4000');
 window.socket = socket;
 
 function App() {
